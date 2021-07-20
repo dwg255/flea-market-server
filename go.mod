a5c74fef904aa7744711de9720613476d5765f62
ljw <445874166@qq.com>
2021-07-20 15:30:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module flea-market
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.7.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"code": 0, "msg": "it works"})
+	})
+	router.Run(":8080")
+}
